83bf6e499f1f8b42393241a111c6f8bf1bf380c1
Fearester2008 <131034025+Fearester2008@users.noreply.github.com>
2023-04-26 16:34:32 +0300

    Update FlixText.hx

--- source/text/FlixText.hx
+++ source/text/FlixText.hx
@@ -12,6 +12,8 @@ class FlixText extends FlxSpriteGroup {
     public var targetY:Float = 0;
     public var isMenu:Bool = false;
     public var theXPos:Float = 0;
+    public var changedX:Bool = false;
+    public var changedY:Bool = true;
 
     public function new(x:Float, y:Float, text:String = '', size:Int, color:FlxColor, alignment:FlxTextAlign) {
         super();
@@ -26,8 +28,14 @@ class FlixText extends FlxSpriteGroup {
         if (isMenu)
             {    
                 var lerpVal:Float = CoolUtil.boundTo(elapsed * 9.6, 0, 1);
+                if(changedY)
+                {
                 theText.y = FlxMath.lerp(theText.y, 200 + (scaledY * 30) + (FlxG.height * 0.1), lerpVal);
+                }
+                if(changedX)
+                {
                 theText.x = FlxMath.lerp(theText.x, theXPos, CoolUtil.boundTo(elapsed * 32, 0, 1));	
+                }
             }
         super.update(elapsed);
     }
