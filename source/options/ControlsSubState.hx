e4aafdeed191a553cc54c058cdd7b2d1f3f7c5e4
Fearester2008 <131034025+Fearester2008@users.noreply.github.com>
2023-08-12 01:16:15 +0300

    Update ControlsSubState.hx

--- source/options/ControlsSubState.hx
+++ source/options/ControlsSubState.hx
@@ -134,7 +134,12 @@ class ControlsSubState extends MusicBeatSubstate {
 
 			if (controls.BACK) {
 				ClientPrefs.reloadControls();
+				#if android
+				FlxTransitionableState.skipNextTransOut = true;
+				FlxG.resetState();
+				#else
 				close();
+				#end
 				FlxG.sound.play(Paths.sound('cancelMenu'));
 			}
 
