7519f48f92575a05a3897dd4677ceb29b9ff7775
Fearester2008 <131034025+Fearester2008@users.noreply.github.com>
2023-08-13 09:28:38 +0300

    Update VersionStuff.hx

--- source/VersionStuff.hx
+++ source/VersionStuff.hx
@@ -8,6 +8,6 @@ class VersionStuff
 {
 public static var altEngineVersion:String = '4.0'; //This is also used for Discord RPC
 public static var psychEngineVersion:String = '0.6.3'; //This is also used for Discord RPC
-public static var updateVersion:String = '';
+public static var updateVersion:String = 'b';
 public static var stage:String = '';
 }
