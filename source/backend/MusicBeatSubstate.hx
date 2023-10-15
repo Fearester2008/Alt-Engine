2100bd88b0b05439e054435b415e0db6a6354f4c
Fearester2008 <131034025+Fearester2008@users.noreply.github.com>
2023-10-15 15:23:59 +0300

    Fixes

--- source/backend/MusicBeatSubstate.hx
+++ source/backend/MusicBeatSubstate.hx
@@ -33,38 +33,52 @@ class MusicBeatSubstate extends FlxSubState
 	#if android
 	var _virtualpad:FlxVirtualPad;
 	var trackedinputsUI:Array<FlxActionInput> = [];
-	var trackedinputsNOTES:Array<FlxActionInput> = [];
-	#end
-	
-	#if android
-	public function addVirtualPad(?DPad:FlxDPadMode, ?Action:FlxActionMode) {
-		_virtualpad = new FlxVirtualPad(DPad, Action, 0.75, ClientPrefs.globalAntialiasing);
+
+	public function addVirtualPad(DPad:FlxDPadMode, Action:FlxActionMode)
+	{
+		_virtualpad = new FlxVirtualPad(DPad, Action);
 		add(_virtualpad);
+
 		controls.setVirtualPadUI(_virtualpad, DPad, Action);
 		trackedinputsUI = controls.trackedinputsUI;
 		controls.trackedinputsUI = [];
 	}
-	#end
-       
-	#if android
-	public function removeVirtualPad() {
-		controls.removeFlxInput(trackedinputsUI);
-		remove(_virtualpad);
-	}
-	#end
 
-	#if android
-		public function addPadCamera() {
-		var camcontrol = new flixel.FlxCamera();
-		camcontrol.bgColor.alpha = 0;
-		FlxG.cameras.add(camcontrol, false);
-		_virtualpad.cameras = [camcontrol];
+	public function removeVirtualPad()
+	{
+		if (trackedinputsUI != [])
+			controls.removeFlxInput(trackedinputsUI);
+
+		if (virtualPad != null)
+			remove(_virtualpad);
+	}
+
+	public function addPadCamera()
+	{
+		if (_virtualpad != null)
+		{
+			var camControls = new flixel.FlxCamera();
+			FlxG.cameras.add(camControls, false);
+			camControls.bgColor.alpha = 0;
+			_virtualpad.cameras = [camControls];
+		}
 	}
 	#end
-	
 	override function destroy() {
+                #if android
+		if (trackedinputsUI != [])
+			controls.removeFlxInput(trackedinputsUI);
+		#end
 
 		super.destroy();
+
+		#if android
+		if (_virtualpad != null)
+		{
+			_virtualpad = FlxDestroyUtil.destroy(_virtualpad);
+			_virtualpad = null;
+		}
+		#end
 	}
 
 	override function update(elapsed:Float)
