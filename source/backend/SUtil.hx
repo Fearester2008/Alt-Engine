f3149347d0b06f45738d04a9d202163bf8086201
Fearester2008 <131034025+Fearester2008@users.noreply.github.com>
2024-05-23 20:05:24 +0300

    Update SUtil.hx

--- source/backend/SUtil.hx
+++ source/backend/SUtil.hx
@@ -14,10 +14,8 @@ import haxe.CallStack;
 import haxe.io.Path;
 import sys.FileSystem;
 import sys.io.File;
-
-#if flash
 import flash.system.System;
-#end
+
 /**
  * ...
  * @author: Saw (M.A. Jigsaw)
