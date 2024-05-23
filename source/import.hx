bf9d2bfa163856135771683a2bb2222ab4bc2072
Fearester2008 <131034025+Fearester2008@users.noreply.github.com>
2024-05-23 19:43:09 +0300

    Update import.hx

--- source/import.hx
+++ source/import.hx
@@ -21,17 +21,6 @@ import mobile.flixel.FlxVirtualPad;
 import mobile.flixel.FlxVirtualPadExtra;
 import mobile.flixel.input.FlxMobileInputID;
 
-//Android
-#if android
-import android.content.Context as AndroidContext;
-import android.widget.Toast as AndroidToast;
-import android.os.Environment as AndroidEnvironment;
-import android.Permissions as AndroidPermissions;
-import android.Settings as AndroidSettings;
-import android.Tools as AndroidTools;
-import android.os.BatteryManager as AndroidBatteryManager;
-#end
-
 #if sys
 import sys.*;
 import sys.io.*;
