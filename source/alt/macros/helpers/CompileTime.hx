2691d36bba64112e047c54bf329a30f3bdb186b9
Fearester2008 <131034025+Fearester2008@users.noreply.github.com>
2024-07-03 17:53:57 +0300

    Update CompileTime.hx

--- source/alt/macros/helpers/CompileTime.hx
+++ source/alt/macros/helpers/CompileTime.hx
@@ -2,6 +2,6 @@ package alt.macros.helpers;
 
 class CompileTime
 {
-  public static var startTime:Date;
-  public static var endTime:Date;
+  public static var startTime:Date.now();
+  public static var endTime:Date.now();
 }
