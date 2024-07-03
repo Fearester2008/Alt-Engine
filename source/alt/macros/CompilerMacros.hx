58e2978759299b097b38ce9cabd3d53d9fb9e271
Fearester2008 <131034025+Fearester2008@users.noreply.github.com>
2024-07-03 09:21:25 +0300

    Update CompilerMacros.hx

--- source/alt/macros/CompilerMacros.hx
+++ source/alt/macros/CompilerMacros.hx
@@ -1,16 +1,34 @@
 package alt.macros;
 import backend.utils.AppController;
+import flixel.FlxG;
 
-//thanks for slushi_ds for this code
+using StringTools;
+
 class CompilerMacros
 {
+    static var compileTime:Float = 0;
     #if macro
     static var ENGINE_VERSION = AppController.altEngineVersion + AppController.stage;
-
     public static function init() {
-        Sys.println('\n---- \033[96mAlt Engine\033[0m version: \x1b[38;5;236m[\033[0m\033[96m${ENGINE_VERSION}\033[0m\x1b[38;5;236m]\033[0m ----');
+        compileTime += FlxG.elapsed;
+        if(AppController.stage == 'b' || AppController.stage == 'beta')
+            Sys.println('This is Beta build. This version can contains bugs, unfinished stuffs...');
+        else if(AppController.stage == 'a' || AppController.stage == 'alpha')
+            Sys.println('This is Alpha build. This version on release stage...');
+        else if(AppController.stage == 'h' || AppController.stage == 'hotfix')
+            Sys.println('This is Hotfix build. This version for new stuffs that will be in next release...');
+        else
+            Sys.println('You use stable build. Enjoy:)');
+
+        Sys.println('\n\n---- \033[96mAlt Engine\033[0m version: \x1b[38;5;236m[\033[0m\033[96m${ENGINE_VERSION}\033[0m\x1b[38;5;236m]\033[0m ----');
         Sys.println('Trying to initialize the compilation...');
         Sys.println('Date on start compilation: \033[32m${Date.now().toString()}\033[0m');
+
+        initAfterCompile();
     }
     #end
+    public static function initAfterCompile()
+    {
+        Sys.println('Compiled Successfully in: \033[32m${compileTime}\033[0m');
+    }
 }
