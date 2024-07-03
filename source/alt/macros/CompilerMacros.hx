06e076f003e10463a4f966b6ab9136549623bb79
Fearester2008 <131034025+Fearester2008@users.noreply.github.com>
2024-07-03 09:25:09 +0300

    Update CompilerMacros.hx

--- source/alt/macros/CompilerMacros.hx
+++ source/alt/macros/CompilerMacros.hx
@@ -1,6 +1,5 @@
 package alt.macros;
 import backend.utils.AppController;
-import flixel.FlxG;
 
 using StringTools;
 
@@ -10,7 +9,7 @@ class CompilerMacros
     #if macro
     static var ENGINE_VERSION = AppController.altEngineVersion + AppController.stage;
     public static function init() {
-        compileTime += FlxG.elapsed;
+        compileTime += 1;
         if(AppController.stage == 'b' || AppController.stage == 'beta')
             Sys.println('This is Beta build. This version can contains bugs, unfinished stuffs...');
         else if(AppController.stage == 'a' || AppController.stage == 'alpha')
@@ -22,7 +21,7 @@ class CompilerMacros
 
         Sys.println('\n\n---- \033[96mAlt Engine\033[0m version: \x1b[38;5;236m[\033[0m\033[96m${ENGINE_VERSION}\033[0m\x1b[38;5;236m]\033[0m ----');
         Sys.println('Trying to initialize the compilation...');
-        Sys.println('Date on start compilation: \033[32m${Date.now().toString()}\033[0m');
+        Sys.println('Date on start compilation: \033[32m${Date.now().toString()}\033[0m s');
 
         initAfterCompile();
     }
